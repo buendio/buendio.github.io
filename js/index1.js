797c1374987a475331ace7cd6ab6a8fe7e6e2159
buendio <bak1981@mail.ru>
2020-11-05 23:48:02 +0200

    refuck

--- js/index1.js
+++ js/index1.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const search = document.querySelector('.search');
   const data_id = document.querySelector('#data_id');
   const data_name = document.querySelector('#data_name');
-  let pagination = document.querySelector('#pagination');
- 
+  const pagination = document.querySelector('#pagination');
+  let notesOnPages = 10;
 
   const getGoods = (handler, filter) => {
     fetch('https://5fa00a5ae21bab0016dfcc6a.mockapi.io/api/payoola/test').then((response) => {
@@ -13,16 +13,16 @@ document.addEventListener('DOMContentLoaded', () => {
       
      }).then(filter).then(handler);
   }
-  
-  const renderCard = (item) => {
-    // pagina
-    let notesOnPage = 10;
-    let pagination_element = []
-    let countOfItems = Math.ceil( item.length / notesOnPage ); 
-    goodsWrapper.textContent='';
 
-    if(item.length) {
-      
+  const renderCard = (item) => {
+    let notesOnPage = notesOnPages;
+    let pagination_element = []
+    let countOfItems = Math.ceil( item.length / notesOnPage );
+    
+    
+    goodsWrapper.textContent='';
+    
+    if(item.length) {      
       item.slice(0, notesOnPage).forEach(element => {       
         goodsWrapper.appendChild(creatCard(element[1], element.name, element["2020-11-02"], element.avatar));
       });
@@ -31,53 +31,67 @@ document.addEventListener('DOMContentLoaded', () => {
       goodsWrapper.textContent = 'Результатов не найдено ❗ ';
     } 
 
-
-
-    
-    let showPage = (e) => {
-      let active = document.querySelector('#pagination li.active');
-        if (active) {
-          active.classList.remove('active');
+        //+++ add pagina    
+        for (let index = 1; index <= countOfItems; index++) {
+          let li = document.createElement('li');
+          li.innerHTML = index;
+          pagination.appendChild(li)
+          pagination_element.push(li); 
         }
-        e.target.classList.add('active');
-        let pageNum = +e.target.textContent;
-        let start = (pageNum - 1)*notesOnPage;
-        let end = start + notesOnPage;
-        let notes = item.slice(start, end);
-        console.log(notes);
-        goodsWrapper.textContent='';
-    if(notes.length) {
-      notes.forEach(element => {
-        goodsWrapper.appendChild(creatCard(element[1], element.name, element["2020-11-02"], element.avatar));
-      });
-    }
-    else {
-      goodsWrapper.textContent = 'Результатов не найдено ❗ ';
-    } 
-    }
+        if (pagination_element.length) {
+          pagination_element[0].classList.add('active');
+        }
+        //--- add pagina
 
+      for (let items of pagination_element) {
+        items.addEventListener('click', showPage)     
+      }
 
-      for (let index = 1; index <= countOfItems; index++) {
-        let li = document.createElement('li');
-        li.innerHTML = index;
-        pagination.appendChild(li)
-        pagination_element.push(li); 
-       }
-
-
-
- 
-    for (let items of pagination_element) {
-      items.addEventListener('click', (e) => {
-        showPage(e);
-      })
-      
-    }
-    
   } 
-   
+  
+ const renderSort = (item) => {
+   console.log(renderCard.getPagination_element);
+  let notesOnPage = notesOnPages;
+  goodsWrapper.textContent='';
+  if(item.length) {      
+    item.slice(0, notesOnPage).forEach(element => {       
+      goodsWrapper.appendChild(creatCard(element[1], element.name, element["2020-11-02"], element.avatar));
+    });
+  }
+  else {
+    goodsWrapper.textContent = 'Результатов не найдено ❗ ';
+  } 
+ }
 
+  const showPage = (e) => {
+    let notesOnPage = notesOnPages;
+    
 
+    let active = document.querySelector('#pagination li.active');
+      if (active) {
+        active.classList.remove('active');
+      }
+      e.target.classList.add('active');
+      let pageNum = +e.target.textContent;
+      let start = (pageNum - 1)*notesOnPage;
+      let end = start + notesOnPage;
+      
+      getGoods( item => {
+          
+          let notes = item.slice(start, end);
+          console.log(notes);
+          goodsWrapper.textContent='';
+          if(notes.length) {
+            notes.forEach(element => {
+              goodsWrapper.appendChild(creatCard(element[1], element.name, element["2020-11-02"], element.avatar));
+            });
+          }
+          else {
+              goodsWrapper.textContent = 'Результатов не найдено ❗ ';
+          } 
+        });
+      
+  }   
   const creatCard = (id, title, price, img) => {
     const card = document.createElement('div');
     card.className = 'data_item';
@@ -89,7 +103,6 @@ document.addEventListener('DOMContentLoaded', () => {
   `
   return card;
   }
-
 
   const searchItem = e => {
     e.preventDefault();
@@ -107,25 +120,43 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
   const sort1 = (e) => {
+    let active = document.querySelector('#pagination li.active');
+    let li = document.querySelectorAll('#pagination li');
+   
+
     const target = e.target;
     if (target.classList.contains('up')) {
+      if (active) {
+        active.classList.remove('active');
+      }
+      li[li.length - 1].classList.add('active');
       target.classList = 'data_id down';
-      getGoods(renderCard, item => {
+      getGoods(renderSort, item => {
         return  item.sort( (a,b) =>   b[1] - a[1] );
        });
     } else {
+      if (active) {
+        active.classList.remove('active');
+      }
+      li[0].classList.add('active');
       target.classList = 'data_id up';
-      getGoods(renderCard, item => {
+      getGoods(renderSort, item => {
         return  item.sort( (a,b) =>   a[1] - b[1] );
        });
     }    
   }
   const sort2 = (e) => {
+    let active = document.querySelector('#pagination li.active');
+    let li = document.querySelectorAll('#pagination li');
     const target = e.target;
     if (target.classList.contains('up')) {
+      if (active) {
+        active.classList.remove('active');
+      }
+      li[0].classList.add('active');
       target.classList = 'data_name down';
       if (data_id.classList.contains('up')) {data_id.classList = 'down data_id'}
-      getGoods(renderCard, item => {
+      getGoods(renderSort, item => {
         return  (
           item.sort( (a,b) =>  {
             if (a.name.toLowerCase() < b.name.toLowerCase())
@@ -135,7 +166,7 @@ document.addEventListener('DOMContentLoaded', () => {
        });
     } else {
       target.classList = 'data_name up';
-      getGoods(renderCard, item => {
+      getGoods(renderSort, item => {
         return  item.sort( (a,b) => {
          
           return 1
@@ -144,15 +175,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }    
   }
    
-
-
   getGoods(renderCard);
   
   search.addEventListener('submit', searchItem);
   data_id.addEventListener('click', sort1);
   data_name.addEventListener('click', sort2);
 
-// MENU
+///////////////////////////////////////////////////// MENU
  $('.nav_mob').hide();//?
       $('.mob').on('click', function(){
         $('.nav_mob').slideToggle();
