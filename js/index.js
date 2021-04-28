46226a51954fb59980ca933e393dff5f5514271a
buendio <mhvatov@gmail.com>
2021-04-28 09:31:34 +0300

    corma

--- js/index.js
+++ js/index.js
@@ -17,7 +17,35 @@ $(document).ready(function(){
   $('.close').on('click', function(e){
     $('.modal').toggleClass('active');
    });
+//////////////////////range slider///////////////////////////////////
+  const slider = document.getElementById('rangeSlider');
 
+  noUiSlider.create(slider, {
+      start: [0, 500],
+      connect: true,
+      step:1,
+      range: {
+          'min': 0,
+          'max': 1000
+      }
+  });
+  const input0 = document.getElementById('inpt1');
+	const input1 = document.getElementById('inpt2');
+	const inputs = [input0, input1];
+  slider.noUiSlider.on('update', function(values, handle){
+		inputs[handle].value = Math.round(values[handle]);
+	});
+  const setSlider = (i, value) => {
+		let arr = [null, null];
+		arr[i] = value;
+
+		slider.noUiSlider.set(arr);
+	};
+  inputs.forEach((el, index) => {
+		el.addEventListener('change', (e) => {
+			setSlider(index, e.currentTarget.value);
+		});
+	});
   /////////////////SCROLL////////////////////////////////
   //  $(".nav, .nav_mob").on("click","a", function (event) {
   //   //отменяем стандартную обработку нажатия по ссылке
