4c18cf9c152aca451a5619888666c1f0cebec935
buendio <mhvatov@gmail.com>
2021-05-09 22:37:08 +0300

    Miramax

--- js/index.js
+++ js/index.js
@@ -1,80 +1,45 @@
 $(document).ready(function(){
-
+  ///////////////scroll header////////////////////////
+	// $(window).scroll(function() {
+	// 	if ($(this).scrollTop() > 1){
+  //    		$('header').addClass("sticky");
+  //  		}
+  // 		 else{
+  //   			 $('header').removeClass("sticky");
+  // 			 }
+	// });
+  ///////////////scroll header/////////////////////////
 
   ////////////////mob nav//////////////////////////////
-	$('.nav_mob').hide();//?
-     $('.mob').on('click', function(){
-       $('.nav_mob').slideToggle();
-       $('.mob span').toggleClass('active');
-       $('body').toggleClass('vh');
-      });
+	// $('.nav_mob').hide();//?
+  //    $('.mob').on('click', function(){
+  //      $('.nav_mob').slideToggle();
+  //      $('.mob span').toggleClass('active');
+  //     });
   /////////////////mob nav///////////////////////////////
-  $('.btnJs').on('click', function(e){
-    e.preventDefault();
-    $('.modal').toggleClass('active');
-    $('body,html').animate({scrollTop: 0}, 1000);
-   });
-  $('.close').on('click', function(e){
-    $('.modal').toggleClass('active');
-   });
-//////////////////////////////////////////////////
-$('.closeAside').on('click', function(e){
-  $('aside').removeClass('active');
-  $('aside').toggleClass('hide');
- });
- $('.products__btn').on('click', function(e){
-  $('aside').removeClass('hide');
-  $('aside').toggleClass('active');
- });
-///////////////////////////////////////////////////
-  /////////////////slickslider////////////////////////////////
-  $('.sliderFor').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.sliderNav',
-    responsive: [
-      {
-        breakpoint: 850,
-        settings: {  
-          dots:true
-        }          
-      }
-    ]
-  });
-  $('.sliderNav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.sliderFor',
-    arrows: true,
-    prevArrow:'<button class="prev"></button>',
-    nextArrow:'<button class="next"></button>',
-    focusOnSelect: true,
-    responsive: [
-      {
-        breakpoint: 850,
-        settings: 'unslick'        
-      }
-    ]
-  });
- //////////////////////TABS//////////////////////////////////////
- $("#tabs-1").show();
- $("#items li").click(function() {
-   if ($(this).attr("class") == "active") {
-     return false;
-   }
-   var link = $(this)
-     .children()
-     .attr("href");
-   var prevActive = $("li.active")
-     .children()
-     .attr("href"); 
-   $("li.active").removeClass("active");
-   $(this).addClass("active");
-   $(prevActive).fadeOut(0, function() {
-     $(link).fadeIn(0);
-   });
-   return false;
- });
+
+  /////////////////SCROLL////////////////////////////////
+  //  $(".nav, .nav_mob").on("click","a", function (event) {
+  //   //отменяем стандартную обработку нажатия по ссылке
+  //   event.preventDefault();
+
+  //   //забираем идентификатор бока с атрибута href
+  //   var id  = $(this).attr('href'),
+
+  //   //узнаем высоту от начала страницы до блока на который ссылается якорь
+  //     top = $(id).offset().top;
+    
+  //   //анимируем переход на расстояние - top за 1500 мс
+  //   $('body,html').animate({scrollTop: top}, 1500);
+  // });
+  ////////////////END SCROLL////////////////////////////////
+
+  ////////////////ADD CLASS/////////////////////////////////
+  // $("#selectBackground ul li a").click(function(e) {
+  //   e.preventDefault();
+  //   $("#selectBackground ul li a").removeClass('active');
+  //   $(this).addClass('active');
+  // });
+  ///////////////END ADD CLASS//////////////////////////////
+
 }); // end ready 
