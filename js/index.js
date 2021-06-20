c60f0b66d848b333eda455a530cd32298f248e9f
buendio <mhvatov@gmail.com>
2021-06-20 19:34:53 +0300

    ---

--- js/index.js
+++ js/index.js
@@ -1,137 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-////////////////mob nav//////////////////////////////
-$('.nav__mob').hide();//?
-$('.mob').on('click', function(){
-  $('.nav__mob').slideToggle();
-  $('.mob span').toggleClass('active');
- // $('body').toggleClass('vh');
- });
+  ///////////////scroll header////////////////////////
+	// $(window).scroll(function() {
+	// 	if ($(this).scrollTop() > 1){
+  //    		$('header').addClass("sticky");
+  //  		}
+  // 		 else{
+  //   			 $('header').removeClass("sticky");
+  // 			 }
+	// });
+  ///////////////scroll header/////////////////////////
 
+  ////////////////mob nav//////////////////////////////
+	// $('.nav__mob').hide();//?
+  //    $('.mob').on('click', function(){
+  //      $('.nav__mob').slideToggle();
+  //      $('.mob span').toggleClass('active');
+  //     });
+  /////////////////mob nav///////////////////////////////
 
-   ///////////////scroll header////////////////////////
-   let header = $('header'),
-   scrollPrev = 0;
+  /////////////////SCROLL////////////////////////////////
+  //  $(".nav, .nav_mob").on("click","a", function (event) {
+  //   //отменяем стандартную обработку нажатия по ссылке
+  //   event.preventDefault();
 
- $(window).scroll(function() {
-   let scrolled = $(window).scrollTop();
+  //   //забираем идентификатор бока с атрибута href
+  //   var id  = $(this).attr('href'),
 
-   if ( scrolled > 0 ) {
-     header.addClass('out');
-   } else {
-     header.removeClass('out');
+  //   //узнаем высоту от начала страницы до блока на который ссылается якорь
+  //     top = $(id).offset().top;
 
-   }
-   scrollPrev = scrolled;
- });
-/////////////////////////////////////////////
-  const inputs = document.querySelectorAll('.inp');
-  const form = document.querySelector('form');
-  const formInputs = form.elements;
+  //   //анимируем переход на расстояние - top за 1500 мс
+  //   $('body,html').animate({scrollTop: top}, 1500);
+  // });
+  ////////////////END SCROLL////////////////////////////////
 
-  inputs.forEach(input => {
-    input.addEventListener('focusin', e => {
-      e.target.closest('.inpWr').classList.remove('error');
-      e.target.closest('.inpWr').classList.remove('full');
-      e.target.closest('.inpWr').classList.add('focus')
-    })
-    input.addEventListener('focusout', e => {
-      if(e.target.closest('.inpWr').classList.contains('focus'))
-      e.target.closest('.inpWr').classList.remove('focus')
-      if (e.target.value) {
-        e.target.closest('.inpWr').classList.add('full')
-      } else {
-        e.target.closest('.inpWr').classList.remove('full')
-      }
-    })
-  })
+  ////////////////ADD CLASS/////////////////////////////////
+  // $("#selectBackground ul li a").click(function(e) {
+  //   e.preventDefault();
+  //   $("#selectBackground ul li a").removeClass('active');
+  //   $(this).addClass('active');
+  // });
+  ///////////////END ADD CLASS//////////////////////////////
 
-  form.addEventListener('submit', event => {
-    [...formInputs].forEach(e => {
-      if(e.type != "submit" && e.type != "checkbox") {
-        e.closest('.inpWr').classList.remove('error');
-        e.closest('.inpWr').classList.remove('focus');
-        if (e.value === '') {
-          event.preventDefault();
-          e.closest('.inpWr').classList.add('error');
-        }
-      }
-    })
-    if($('.new-select').text() == 'Місяць') {
-      console.log('111');
-      $('.new-select').addClass('error');
-    }
-  })
-
-//////////////////STYLE FOR SELECT///////////////////////////////////////////////
-
-///////////////////////////////////////////////
-$('.select').each(function() {
-  const _this = $(this),
-      selectOption = _this.find('option'),
-      selectOptionLength = selectOption.length,
-      selectedOption = selectOption.filter(':selected'),
-      duration = 450; // длительность анимации
-
-  _this.hide();
-  _this.wrap('<div class="select"></div>');
-  $('<div>', {
-      class: 'new-select',
-      text: _this.children('option:disabled').text()
-  }).insertAfter(_this);
-
-  const selectHead = _this.next('.new-select');
-  $('<div>', {
-      class: 'new-select__list'
-  }).insertAfter(selectHead);
-
-  const selectList = selectHead.next('.new-select__list');
-  for (let i = 1; i < selectOptionLength; i++) {
-      $('<div>', {
-          class: 'new-select__item',
-          html: $('<span>', {
-              text: selectOption.eq(i).text()
-          })
-      })
-      .attr('data-value', selectOption.eq(i).val())
-      .appendTo(selectList);
-  }
-
-  const selectItem = selectList.find('.new-select__item');
-  selectList.slideUp(0);
-  selectHead.on('click', function() {
-      if ( !$(this).hasClass('on') ) {
-          $(this).addClass('on');
-          selectList.slideDown(duration);
-
-          selectItem.on('click', function() {
-              let chooseItem = $(this).data('value');
-
-              $('select').val(chooseItem).attr('selected', 'selected');
-              selectHead.text( $(this).find('span').text() );
-
-              selectList.slideUp(duration);
-              selectHead.removeClass('on');
-          });
-
-      } else {
-          $(this).removeClass('on');
-          selectList.slideUp(duration);
-      }
-  });
-});
-console.log($('.new-select').text());
-$('.new-select__item').on('click', function() {
-  $('.new-select').removeClass('focus');
-  $('.new-select').removeClass('error');
-  $('.new-select').addClass('full');
-  $(this).parent().parent().parent().addClass('full');
-})
-
-$('.new-select').on('click', function() {
-
-  $(this).parent().parent().addClass('focus');
-  $(this).toggleClass('focus')
-})
-//////////////////////MASKA PHONE///////////////////////
-$(".inpPhone").mask("+38(999) 999-99-99");
 }); // end DOMContentLoaded
