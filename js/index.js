660386cf7f32cf91ebd888b499bbf6324c4c1484
buendio <mhvatov@gmail.com>
2021-06-25 14:37:29 +0300

    ---

--- js/index.js
+++ js/index.js
@@ -8,36 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   ///////////////scroll header/////////////////////////
 
-  ////////////////mob nav//////////////////////////////
-	// $('.nav__mob').hide();//?
-  //    $('.mob').on('click', function(){
-  //      $('.nav__mob').slideToggle();
-  //      $('.mob span').toggleClass('active');
-  //     });
-  /////////////////mob nav///////////////////////////////
-
-  /////////////////SCROLL////////////////////////////////
-  //  $(".nav, .nav_mob").on("click","a", function (event) {
-  //   //отменяем стандартную обработку нажатия по ссылке
-  //   event.preventDefault();
-
-  //   //забираем идентификатор бока с атрибута href
-  //   var id  = $(this).attr('href'),
-
-  //   //узнаем высоту от начала страницы до блока на который ссылается якорь
-  //     top = $(id).offset().top;
-
-  //   //анимируем переход на расстояние - top за 1500 мс
-  //   $('body,html').animate({scrollTop: top}, 1500);
-  // });
-  ////////////////END SCROLL////////////////////////////////
-
-  ////////////////ADD CLASS/////////////////////////////////
-  // $("#selectBackground ul li a").click(function(e) {
-  //   e.preventDefault();
-  //   $("#selectBackground ul li a").removeClass('active');
-  //   $(this).addClass('active');
-  // });
-  ///////////////END ADD CLASS//////////////////////////////
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 72){
+     		$('header').addClass("sticky");
+   		}
+  		 else{
+    			 $('header').removeClass("sticky");
+  			 }
+	});
 
 }); // end DOMContentLoaded
